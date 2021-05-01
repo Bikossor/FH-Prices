67f45abef641ed0c7ccaca7c722ec596dca6ffcf
Bikossor <andre.lichtenthaeler@gmail.com>
2021-05-01 21:18:56 +0200

    Put in the AppBar
    #61

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchAppBar from './components/SearchAppBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+      <SearchAppBar />
+      <main>
+        
+      </main>
     </div>
   );
 }
