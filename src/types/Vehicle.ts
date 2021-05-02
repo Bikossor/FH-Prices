b4ac925526ec748d67160383c71be04742466269
Bikossor <andre.lichtenthaeler@gmail.com>
2021-05-02 03:41:17 +0200

    Prepare the new data
    #67

---
+++ src/types/Vehicle.ts
@@ -0,0 +1,11 @@
+export type Vehicle = {
+  Year: number;
+  Manufacturer: string;
+  Model: string;
+  Type: string;
+  Boost: string;
+  Rarity: string;
+  Price: number | null;
+  Class: string;
+  PI: number;
+};
