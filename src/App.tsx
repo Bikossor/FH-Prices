199075691d6b733d87e2f14238b056e2578595ba
Bikossor <andre.lichtenthaeler@gmail.com>
2021-05-02 03:42:10 +0200

    Load and display the new data
    #67

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import './App.css';
 import SearchAppBar from './components/SearchAppBar';
 
+import { FH4 } from "./data/FH4";
+import EntryCard from './components/EntryCard';
+
 function App() {
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
       <SearchAppBar />
       <main>
-        
+        {FH4.map(car => 
+          <EntryCard
+            manufacturer={car.Manufacturer}
+            model={car.Model}
+            price={car.Price}
+            rarity={car.Rarity}
+            type={car.Type}
+            year={car.Year}
+            key={`${car.Year} ${car.Manufacturer} ${car.Model}`}
+          />
+        )}
       </main>
     </div>
   );
