0d2674e8b3f80ce7edcb0a7584d7280224ef3ee7
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2021-07-11 01:24:40 +0200

    Make the AppBar fixed in position
    #101

--- src/components/SearchAppBar.tsx
+++ src/components/SearchAppBar.tsx
@@ -90,7 +90,7 @@ export default function SearchAppBar({ onSearchChange }: SearchAppBarProps) {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="fixed">
         <Toolbar>
           <IconButton
             edge="start"
