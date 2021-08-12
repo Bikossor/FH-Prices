b4d8ed2c11c8d6f2928ff84b7bf5069c5bf15cba
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2021-08-13 01:01:55 +0200

    Introduce the dark mode (#112)
    * Set up dark mode (#111)
    * Improve the secondary color in dark mode (#111)

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import { Entries, EntryFilterAtom } from "./Atoms";
 import { MenuDrawer } from "./components/MenuDrawer";
 import { AboutDialog } from "./components/AboutDialog";
 import { useEffect } from "react";
+import { createTheme, CssBaseline, ThemeProvider, useMediaQuery } from "@material-ui/core";
+import { grey, pink } from "@material-ui/core/colors";
 
 const filterEntries = (searchText: string) =>
   FH4.filter(
@@ -20,32 +22,52 @@ const filterEntries = (searchText: string) =>
 function App() {
   const [entries, setEntries] = useRecoilState(Entries);
   const { searchText } = useRecoilValue(EntryFilterAtom);
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+          primary: {
+            main: grey["900"],
+          },
+          secondary: {
+            main: prefersDarkMode ? pink["200"] : pink["A400"]
+          },
+        },
+      }),
+    [prefersDarkMode],
+  );
 
   useEffect(() => {
     setEntries(filterEntries(searchText.toLowerCase()));
   }, [searchText])
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
-      <SearchAppBar />
-      <main style={{ display: "flex", justifyContent: "center" }}>
-        <div style={{ minWidth: "50%", marginTop: "4rem" }}>
-          {entries.map((car, index) => (
-            <EntryCard
-              manufacturer={car.Manufacturer}
-              model={car.Model}
-              price={car.Price}
-              rarity={car.Rarity}
-              type={car.Type}
-              year={car.Year}
-              key={index}
-            />
-          ))}
-        </div>
-      </main>
-      <MenuDrawer />
-      <AboutDialog />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <SearchAppBar />
+        <main style={{ display: "flex", justifyContent: "center" }}>
+          <div style={{ minWidth: "50%", marginTop: "4rem" }}>
+            {entries.map((car, index) => (
+              <EntryCard
+                manufacturer={car.Manufacturer}
+                model={car.Model}
+                price={car.Price}
+                rarity={car.Rarity}
+                type={car.Type}
+                year={car.Year}
+                key={index}
+              />
+            ))}
+          </div>
+        </main>
+        <MenuDrawer />
+        <AboutDialog />
+      </div>
+    </ThemeProvider>
   );
 }
 
