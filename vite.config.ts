776b1d719bd7f48c07bde4ee18933bd3a97f5faf
Bikossor <andre.lichtenthaeler@gmail.com>
2025-06-07 01:00:35 +0200

    chore: configure vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
 });
